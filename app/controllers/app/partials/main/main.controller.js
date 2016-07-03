1a5db5a698b4a9be06084de2eb2f61b00787d50b
Nikhil <ns.sikka@gmail.com>
2016-07-03 20:45:52 +0530

    updated ui and refactored code

--- app/controllers/app/partials/main/main.controller.js
+++ app/controllers/app/partials/main/main.controller.js
@@ -3,9 +3,17 @@
     .module('semicolon')
     .controller( 'MainCtrl', controller);
 
-  function controller() {
+  function controller($http, $log) {
     var vm = this;
 
     vm.company = "Semicolon Inc.";
+
+    vm.getTech = function() {
+      $http.get('config/data/tech.json').then(function success(response){
+        vm.technologies = response.data;
+      }, function error(response){
+          $log.error(resonse.statusText);
+      })
+    }
   }
 })();
